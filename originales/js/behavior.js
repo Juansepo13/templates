b4163f2bfeb2fbee6333940d6ea655851b1c9cb7
Juansepo13 <juancho_s1314@hotmail.com>
2022-07-01 12:35:09 -0500

    add all changues

--- originales/js/behavior.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    alert('Hello!');
-    document.getElementById("smart_thumbnail");
-    var thumbnailElement = document.getElementById("smart_thumbnail");
-    thumbnailElement.addEventListener("click", function() {
-	alert("hello Dude");
-    });
-});
-
